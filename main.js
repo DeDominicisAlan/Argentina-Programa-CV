c2a4f918c73ddc1e470b27d4817525cb1692bb6f
alan <dedominicisalan@gmail.com>
2023-01-22 01:32:14 -0300

    Se agrego un navbar responsive y se modificaron algunos elementos en modo responsivo

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const color = document.getElementsByClassName('habilidades-item');
 const boton = document.querySelector('#boton-habilidades');
 const barra = document.getElementById('barra')
+const barra_lista = document.getElementById('barra-lista');
 
 let cambio = false;
 
@@ -17,3 +18,10 @@ boton.addEventListener('click',  () => {
 window.addEventListener('scroll', () =>{
     barra.classList.toggle('barra-scroll', window.scrollY > 680);
 } )
+
+function funcion(){
+    if(barra_lista.classList.value === '')
+        barra_lista.classList.value = 'barra-click'
+    else
+        barra_lista.classList.value = ''
+}
